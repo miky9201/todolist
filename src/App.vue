c0f23f6360e0c997121890434d3cb25908fde175
Mike Baldacci <mikebaldacci@MacBookMike2024.local>
2024-11-23 10:08:49 +0100

    Setting some data

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
 <template>
-  <h1>Todolist</h1>
+  <h1>{{ title }}</h1>
+  <ul>
+    <li :key="task.index" v-for="task in tasks">
+      {{ task }}
+      <button>X</button>
+    </li>
+  </ul>
 </template>
 
-<script></script>
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      title: 'Todolist',
+      tasks: ['Something to do', 'Other thing to do', 'Final thing to do'],
+    }
+  },
+})
+</script>
 
 <style scoped>
 h1 {
